0ae8a816fff6ece75dc2c68c611362afa4078a66
Mohamed Benkedadra <hammicristo@gmail.com>
2018-04-25 18:52:40 +0100

    fixed avatar upload png bug

--- gce_app/functions.py
+++ gce_app/functions.py
@@ -5,15 +5,15 @@ def image_center_crop(img_object):
     # making image compatibale with JPEG FORMAT
     img = Image.new("RGB", img_original.size, (255, 255, 255))
     if img_original.format == 'PNG':
-        img.paste(img_original, mask = img_original.split()[3])
+        img_original = img_original.convert('RGBA')
+        img.paste(img_original, mask = img_original.split()[-1])
     else:
         img = img_original
-    
+    print('after')
     # cropping to a perfect square
     width, height = img.size
     start = width/2 - height/2
     end = width/2 + height/2
-    
     if width != height:
         if width > height:
             crop_region = (start,0,end,height)
